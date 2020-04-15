999553c68163d0f09396003e1021871e6959f62a
mrzhang638993 <1446719386@qq.com>
2020-04-15 18:15:10 +0800

    commit file

--- 进制转换.c
+++ 进制转换.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void)
-{
-	//  对应的执行逻辑和体现的。
-	system("pause");
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(void)
+//{
+//	//  对应的执行逻辑和体现的。
+//	char a=-45;
+//	char b=a>>2;  //对应的左移两位
+//	printf("%d\n",b);  //  -12  对应的是补1的
+//	system("pause");
+//	return 0;
+//}
